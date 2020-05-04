6ebb215558381359adb0ae7e7f265bc6016bcaf0
piupuer <396679132@qq.com>
2020-05-04 12:59:44 +0800

    menu struct

---
+++ pkg/request/sys_menu.go
@@ -0,0 +1,41 @@
+package request
+
+import "go-shipment-api/pkg/response"
+
+// 获取菜单列表结构体
+type MenuListRequestStruct struct {
+	Name              string `json:"name" form:"name"`
+	Title             string `json:"title" form:"title"`
+	Path              string `json:"path" form:"path"`
+	Component         string `json:"component" form:"component"`
+	Redirect          string `json:"redirect"`
+	Status            *bool  `json:"status" form:"status"`
+	Visible           *bool  `json:"visible" form:"visible"`
+	Breadcrumb        *bool  `json:"breadcrumb" form:"breadcrumb"`
+	Creator           string `json:"creator" form:"creator"`
+	response.PageInfo        // 分页参数
+}
+
+// 创建菜单结构体
+type CreateMenuRequestStruct struct {
+	Name       string `json:"name" validate:"required"`
+	Title      string `json:"title"`
+	Icon       string `json:"icon"`
+	Path       string `json:"path"`
+	Redirect   string `json:"redirect"`
+	Component  string `json:"component"`
+	Permission string `json:"permission"`
+	Sort       int    `json:"sort"`
+	Status     *bool  `json:"status"`
+	Visible    *bool  `json:"visible"`
+	Breadcrumb *bool  `json:"breadcrumb"`
+	ParentId   uint   `json:"parentId"`
+	Creator    string `json:"creator"`
+}
+
+// 翻译需要校验的字段名称
+func (s CreateMenuRequestStruct) FieldTrans() map[string]string {
+	m := make(map[string]string, 0)
+	m["Name"] = "菜单名称"
+	return m
+}
