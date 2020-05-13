408c1ef1ea49d02bf1aae8f099b92fc48366e948
piupuer <396679132@qq.com>
2020-05-13 12:37:33 +0800

    bug fix

--- middleware/transaction_test.go
+++ middleware/transaction_test.go
@@ -8,10 +8,11 @@ import (
 	"testing"
 )
 
-func TestException(t *testing.T) {
+func TestTransaction(t *testing.T) {
 	tests.InitTestEnv()
 	// 测试并发访问事务会不会有问题
 	var wg sync.WaitGroup
+	// 这里10000按需设置, 可能会导致mysql崩溃, 建议使用本地数据库
 	for i := 0; i < 10000; i++ {
 		wg.Add(1)
 		go func() {
