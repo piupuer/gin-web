d263eea18da0a0ae26e7835b626692cec611dd8c
piupuer <396679132@qq.com>
2020-08-08 19:42:08 +0800

    add gaode ip real location

--- pkg/utils/address.go
+++ pkg/utils/address.go
@@ -15,7 +15,7 @@ type IpResp struct {
 
 // 获取IP真实地址
 func GetIpRealLocation(ip string) string {
-	resp, err := http.Get(fmt.Sprintf("https://restapi.amap.com/v3/ip?ip=%s&key=%s", ip, "0d1c332f93207b553ad59d6bc72e6e88"))
+	resp, err := http.Get(fmt.Sprintf("https://restapi.amap.com/v3/ip?ip=%s&key=%s", ip, "9130aaac2b7a920b8bbd5dc9647fbe9e"))
 	address := "未知地址"
 	if err != nil {
 		global.Log.Error(fmt.Sprintf("[GetIpRealLocation]IP地址查询失败: %v", err))
@@ -29,12 +29,13 @@ func GetIpRealLocation(ip string) string {
 		return address
 	}
 	// json数据转结构体
-	var json IpResp
-	Json2Struct(string(data), &json)
-	if json.Status == "1" {
-		address = json.Province
-		if json.City != "" {
-			address += json.City
+	var result IpResp
+	Json2Struct(string(data), &result)
+	if result.Status == "1" {
+		address = result.Province
+		// 城市不为空且城市与省份不重复
+		if result.City != "" && result.Province != result.City {
+			address += result.City
 		}
 	}
 	return address
