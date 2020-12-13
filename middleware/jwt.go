fe98a1a451f6617b280ba992f2ce2a26c8784ea5
piupuer <396679132@qq.com>
2020-12-13 16:20:46 +0800

    add refresh token

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -18,7 +18,7 @@ func InitAuth() (*jwt.GinJWTMiddleware, error) {
 		Realm:           global.Conf.Jwt.Realm,                                 // jwt标识
 		Key:             []byte(global.Conf.Jwt.Key),                           // 服务端密钥
 		Timeout:         time.Hour * time.Duration(global.Conf.Jwt.Timeout),    // token过期时间
-		MaxRefresh:      time.Hour * time.Duration(global.Conf.Jwt.MaxRefresh), // token更新时间
+		MaxRefresh:      time.Hour * time.Duration(global.Conf.Jwt.MaxRefresh), // token最大刷新时间(RefreshToken过期时间=Timeout+MaxRefresh)
 		PayloadFunc:     payloadFunc,                                           // 有效载荷处理
 		IdentityHandler: identityHandler,                                       // 解析Claims
 		Authenticator:   login,                                                 // 校验token的正确性, 处理登录逻辑
@@ -26,6 +26,7 @@ func InitAuth() (*jwt.GinJWTMiddleware, error) {
 		Unauthorized:    unauthorized,                                          // 用户登录校验失败处理
 		LoginResponse:   loginResponse,                                         // 登录成功后的响应
 		LogoutResponse:  logoutResponse,                                        // 登出后的响应
+		RefreshResponse: refreshResponse,                                       // 刷新token后的响应
 		TokenLookup:     "header: Authorization, query: token, cookie: jwt",    // 自动在这几个地方寻找请求中的token
 		TokenHeadName:   "Bearer",                                              // header名称
 		TimeFunc:        time.Now,
@@ -64,7 +65,7 @@ func login(c *gin.Context) (interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
-	
+
 	u := &models.SysUser{
 		Username: req.Username,
 		Password: string(decodeData),
@@ -106,7 +107,7 @@ func unauthorized(c *gin.Context, code int, message string) {
 
 func loginResponse(c *gin.Context, code int, token string, expires time.Time) {
 	response.SuccessWithData(map[string]interface{}{
-		"token":   token,
+		"token": token,
 		"expires": models.LocalTime{
 			Time: expires,
 		},
@@ -115,4 +116,13 @@ func loginResponse(c *gin.Context, code int, token string, expires time.Time) {
 
 func logoutResponse(c *gin.Context, code int) {
 	response.Success()
+}
+
+func refreshResponse(c *gin.Context, code int, token string, expires time.Time) {
+	response.SuccessWithData(map[string]interface{}{
+		"token": token,
+		"expires": models.LocalTime{
+			Time: expires,
+		},
+	})
 }
