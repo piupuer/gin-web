f5943c972af22a15dfd9e2535e9ba6542d8613da
piupuer <396679132@qq.com>
2020-12-19 17:15:43 +0800

    avoid utils/global circle

--- pkg/utils/json.go
+++ pkg/utils/json.go
@@ -3,14 +3,13 @@ package utils
 import (
 	"encoding/json"
 	"fmt"
-	"gin-web/pkg/global"
 )
 
 // 结构体转为json
 func Struct2Json(obj interface{}) string {
 	str, err := json.Marshal(obj)
 	if err != nil {
-		global.Log.Debug(fmt.Sprintf("[Struct2Json]转换异常: %v", err))
+		fmt.Println(fmt.Sprintf("[Struct2Json]转换异常: %v", err))
 	}
 	return string(str)
 }
@@ -20,7 +19,7 @@ func Json2Struct(str string, obj interface{}) {
 	// 将json转为结构体
 	err := json.Unmarshal([]byte(str), obj)
 	if err != nil {
-		global.Log.Debug(fmt.Sprintf("[Json2Struct]转换异常: %v", err))
+		fmt.Println(fmt.Sprintf("[Json2Struct]转换异常: %v", err))
 	}
 }
 
