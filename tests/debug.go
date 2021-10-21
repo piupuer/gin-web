8d6ac7a1965fa3b0fcdf4e38738292ec42113984
piupuer <396679132@qq.com>
2021-10-21 16:15:39 +0800

    remove test files

--- tests/debug.go
+++ tests/debug.go
@@ -1,14 +1,8 @@
 package tests
 
-// 初始化单元测试必须的环境, 有些方法无需启动应用先测试
 func InitTestEnv() {
-	// 初始化配置
 	Config()
-
-	// 初始化日志
 	Logger()
-
-	// 初始化数据库
 	Redis()
 	Mysql()
 }
--- tests/redis.go
+++ tests/redis.go
@@ -6,17 +6,16 @@ import (
 	"github.com/piupuer/go-helper/pkg/job"
 )
 
-// 初始化redis数据库
 func Redis() {
 	// parse redis URI
 	client, err := job.ParseRedisURI(global.Conf.Redis.Uri)
 	if err != nil {
-		panic(fmt.Sprintf("[单元测试]初始化redis异常: %v", err))
+		panic(fmt.Sprintf("[unit test]initialize redis failed: %v", err))
 	}
 	err = client.Ping(ctx).Err()
 	if err != nil {
-		panic(fmt.Sprintf("[单元测试]初始化redis异常: %v", err))
+		panic(fmt.Sprintf("[unit test]initialize redis failed: %v", err))
 	}
 	global.Redis = client
-	global.Log.Debug(ctx, "[单元测试]初始化redis完成")
+	global.Log.Debug(ctx, "[unit test]initialize redis success")
 }
