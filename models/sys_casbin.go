950860348b76d48f918d50ae4ea3850c3ca077e9
piupuer <396679132@qq.com>
2021-10-22 17:15:12 +0800

    bug fix

--- models/sys_casbin.go
+++ models/sys_casbin.go
@@ -3,13 +3,13 @@ package models
 // refer to: github.com/casbin/gorm-adapter/v2/adapter.go CasbinRule
 type SysCasbin struct {
 	Id    uint   `gorm:"primaryKey;autoIncrement"`
-	PType string `gorm:"column:ptype;size:100;index:idx_casbin_unique;comment:'enforer type'"`
-	V0    string `gorm:"size:100;index:idx_casbin_unique;comment:'role keyword(SysRole.Keyword)'"`
-	V1    string `gorm:"size:100;index:idx_casbin_unique;comment:'resource name'"`
-	V2    string `gorm:"size:100;index:idx_casbin_unique;comment:'request method'"`
-	V3    string `gorm:"size:100;index:idx_casbin_unique"`
-	V4    string `gorm:"size:100;index:idx_casbin_unique"`
-	V5    string `gorm:"size:100;index:idx_casbin_unique"`
+	PType string `gorm:"column:ptype;size:100;index:idx_casbin,unique;comment:'enforer type'"`
+	V0    string `gorm:"size:100;index:idx_casbin,unique;comment:'role keyword(SysRole.Keyword)'"`
+	V1    string `gorm:"size:100;index:idx_casbin,unique;comment:'resource name'"`
+	V2    string `gorm:"size:100;index:idx_casbin,unique;comment:'request method'"`
+	V3    string `gorm:"size:100;index:idx_casbin,unique"`
+	V4    string `gorm:"size:100;index:idx_casbin,unique"`
+	V5    string `gorm:"size:100;index:idx_casbin,unique"`
 }
 
 // role and casbin
