2ed81e686f96fca86b44ce754d4ab225f4e809e4
piupuer <396679132@qq.com>
2022-03-27 10:47:30 +0800

    remove model comment ''

--- models/sys_role.go
+++ models/sys_role.go
@@ -18,11 +18,11 @@ var SysRoleStatusConst = map[uint]string{
 
 type SysRole struct {
 	ms.M
-	Name    string    `gorm:"comment:'name'" json:"name"`
-	Keyword string    `gorm:"index:idx_keyword,unique;comment:'keyword(unique str)'" json:"keyword"`
-	Desc    string    `gorm:"comment:'description'" json:"desc"`
-	Status  *uint     `gorm:"type:tinyint(1);default:1;comment:'status(0: disabled, 1: enable)'" json:"status"`
-	Sort    *uint     `gorm:"default:1;comment:'sort(>=0, the smaller the sort, the greater the permission, sort=0 is a super admin)'" json:"sort"`
+	Name    string    `gorm:"comment:name" json:"name"`
+	Keyword string    `gorm:"index:idx_keyword,unique;comment:keyword(unique str)" json:"keyword"`
+	Desc    string    `gorm:"comment:description" json:"desc"`
+	Status  *uint     `gorm:"type:tinyint(1);default:1;comment:status(0: disabled, 1: enable)" json:"status"`
+	Sort    *uint     `gorm:"default:1;comment:sort(>=0, the smaller the sort, the greater the permission, sort=0 is a super admin)" json:"sort"`
 	Menus   []uint    `gorm:"-" json:"menus"`
 	Users   []SysUser `gorm:"foreignKey:RoleId" json:"users"`
 }
