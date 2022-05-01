1388a07b1da91151a04bff3250b07b27455bc9c0
piupuer <396679132@qq.com>
2022-05-01 20:26:51 +0800

    use delay.Queue replace dcron

--- initialize/cron.go
+++ initialize/cron.go
@@ -3,83 +3,86 @@ package initialize
 import (
 	"context"
 	"gin-web/pkg/global"
-	"github.com/piupuer/go-helper/pkg/job"
+	"github.com/piupuer/go-helper/pkg/delay"
 	"github.com/piupuer/go-helper/pkg/log"
 	"github.com/piupuer/go-helper/pkg/migrate"
-	"github.com/piupuer/go-helper/pkg/tracing"
+	"github.com/pkg/errors"
 	"os"
 )
 
 func Cron() {
-	j, err := job.New(
-		job.Config{
-			RedisClient: global.Redis,
-		},
-		job.WithCtx(ctx),
-		job.WithAutoRequestId(true),
+	qu := delay.NewQueue(
+		delay.WithQueueName(global.ProName),
+		delay.WithQueueHandler(taskHandler),
 	)
-	if err != nil {
-		panic(err)
+	if qu.Error != nil {
+		panic(errors.Wrap(qu.Error, "initialize cron job failed"))
 	}
 	expr := os.Getenv("CRON_RESET")
 	// expr := "@every 600s"
 	if expr != "" {
-		j.AddTask(job.GoodTask{
-			Name: "reset",
-			Expr: expr,
-			Func: reset,
-		}).Start()
+		err := qu.Cron(
+			delay.WithQueueTaskUuid("cron_reset_task"),
+			delay.WithQueueTaskName("reset"),
+			delay.WithQueueTaskExpr(expr),
+		)
+		if err != nil {
+			panic(errors.Wrap(err, "initialize cron job failed"))
+		}
 	}
 	log.WithContext(ctx).Debug("initialize cron job success")
 }
 
-func reset(ctx context.Context) error {
-	ctx = tracing.NewId(ctx)
-	log.WithContext(ctx).Info("[cron job][reset]starting...")
+func taskHandler(ctx context.Context, t delay.Task) (err error) {
+	switch t.Uid {
+	case "cron_reset_task":
+		log.WithContext(ctx).Info("[cron job][reset]starting...")
 
-	if global.Conf.Redis.EnableBinlog {
-		global.Redis.Del(ctx, []string{
-			global.Conf.Mysql.DSN.DBName + "_tb_fsm_event",
-			global.Conf.Mysql.DSN.DBName + "_tb_fsm_event_item",
-			global.Conf.Mysql.DSN.DBName + "_tb_fsm_event_role_relation",
-			global.Conf.Mysql.DSN.DBName + "_tb_fsm_event_src_item_relation",
-			global.Conf.Mysql.DSN.DBName + "_tb_fsm_event_user_relation",
-			global.Conf.Mysql.DSN.DBName + "_tb_fsm_log",
-			global.Conf.Mysql.DSN.DBName + "_tb_fsm_log_approval_role_relation",
-			global.Conf.Mysql.DSN.DBName + "_tb_fsm_log_approval_user_relation",
-			global.Conf.Mysql.DSN.DBName + "_tb_fsm_machine",
-			global.Conf.Mysql.DSN.DBName + "_tb_fsm_role",
-			global.Conf.Mysql.DSN.DBName + "_tb_fsm_user",
-			global.Conf.Mysql.DSN.DBName + "_tb_leave",
-			global.Conf.Mysql.DSN.DBName + "_tb_sys_api",
-			global.Conf.Mysql.DSN.DBName + "_tb_sys_casbin",
-			global.Conf.Mysql.DSN.DBName + "_tb_sys_dict",
-			global.Conf.Mysql.DSN.DBName + "_tb_sys_dict_data",
-			global.Conf.Mysql.DSN.DBName + "_tb_sys_machine",
-			global.Conf.Mysql.DSN.DBName + "_tb_sys_menu",
-			global.Conf.Mysql.DSN.DBName + "_tb_sys_menu_role_relation",
-			global.Conf.Mysql.DSN.DBName + "_tb_sys_message",
-			global.Conf.Mysql.DSN.DBName + "_tb_sys_message_log",
-			global.Conf.Mysql.DSN.DBName + "_tb_sys_role",
-			global.Conf.Mysql.DSN.DBName + "_tb_sys_user",
-		}...)
-	}
-	tables := make([]string, 0)
-	global.Mysql.Raw("show tables").Scan(&tables)
-	for _, item := range tables {
-		if item == "tb_sys_operation_log" {
-			continue
+		if global.Conf.Redis.EnableBinlog {
+			global.Redis.Del(ctx, []string{
+				global.Conf.Mysql.DSN.DBName + "_tb_fsm_event",
+				global.Conf.Mysql.DSN.DBName + "_tb_fsm_event_item",
+				global.Conf.Mysql.DSN.DBName + "_tb_fsm_event_role_relation",
+				global.Conf.Mysql.DSN.DBName + "_tb_fsm_event_src_item_relation",
+				global.Conf.Mysql.DSN.DBName + "_tb_fsm_event_user_relation",
+				global.Conf.Mysql.DSN.DBName + "_tb_fsm_log",
+				global.Conf.Mysql.DSN.DBName + "_tb_fsm_log_approval_role_relation",
+				global.Conf.Mysql.DSN.DBName + "_tb_fsm_log_approval_user_relation",
+				global.Conf.Mysql.DSN.DBName + "_tb_fsm_machine",
+				global.Conf.Mysql.DSN.DBName + "_tb_fsm_role",
+				global.Conf.Mysql.DSN.DBName + "_tb_fsm_user",
+				global.Conf.Mysql.DSN.DBName + "_tb_leave",
+				global.Conf.Mysql.DSN.DBName + "_tb_sys_api",
+				global.Conf.Mysql.DSN.DBName + "_tb_sys_casbin",
+				global.Conf.Mysql.DSN.DBName + "_tb_sys_dict",
+				global.Conf.Mysql.DSN.DBName + "_tb_sys_dict_data",
+				global.Conf.Mysql.DSN.DBName + "_tb_sys_machine",
+				global.Conf.Mysql.DSN.DBName + "_tb_sys_menu",
+				global.Conf.Mysql.DSN.DBName + "_tb_sys_menu_role_relation",
+				global.Conf.Mysql.DSN.DBName + "_tb_sys_message",
+				global.Conf.Mysql.DSN.DBName + "_tb_sys_message_log",
+				global.Conf.Mysql.DSN.DBName + "_tb_sys_role",
+				global.Conf.Mysql.DSN.DBName + "_tb_sys_user",
+			}...)
 		}
-		global.Mysql.Exec("TRUNCATE TABLE " + item)
+		tables := make([]string, 0)
+		db := global.Mysql.WithContext(ctx)
+		db.Raw("show tables").Scan(&tables)
+		for _, item := range tables {
+			if item == "tb_sys_operation_log" {
+				continue
+			}
+			db.Exec("TRUNCATE TABLE " + item)
+		}
+		// reset data
+		uri := global.Conf.Mysql.Uri
+		migrate.Do(
+			migrate.WithCtx(ctx),
+			migrate.WithUri(uri),
+			migrate.WithFs(sqlFs),
+			migrate.WithFsRoot("db"),
+		)
+		log.WithContext(ctx).Info("[cron job][reset]ended")
 	}
-	// reset data
-	uri := global.Conf.Mysql.Uri
-	migrate.Do(
-		migrate.WithCtx(ctx),
-		migrate.WithUri(uri),
-		migrate.WithFs(sqlFs),
-		migrate.WithFsRoot("db"),
-	)
-	log.WithContext(ctx).Info("[cron job][reset]ended")
-	return nil
+	return
 }
